23c4afc30014b6b93441f067845fd54c411a856f
BhabishyaLC <163528869+BhabishyaLC@users.noreply.github.com>
2024-04-08 12:58:59 +0000

    test file modified

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+const personNumber = 987654321
+let personName = "Bhabishya"
+var personAddress="Ghorahi"
+personId = '12345'
+
+
+personName = "Ayush"
+
+personId = '33434'
+
+// console.log(personNumber);
+// console.log(personName);
+// console.log(personAddress);
+// console.log(personId);
+
+/*donot use 'var' variable*/
+
+console.table([personNumber, personName,personAddress,personId]);
