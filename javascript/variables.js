a26412fcc29bc5fec6b3310c350a0532780ada21
BhabishyaLC <163528869+BhabishyaLC@users.noreply.github.com>
2024-04-08 13:06:57 +0000

    name change

---
+++ javascript/variables.js
@@ -0,0 +1,18 @@
+const personNumber = 987654321
+let personName = "Bhabishya"
+var personAddress="Ghorahi"
+personId = '12345'
+
+
+personName = "Ayush"
+
+personId = '33434'
+
+// console.log(personNumber);
+// console.log(personName);
+// console.log(personAddress);
+// console.log(personId);
+
+/*donot use 'var' variable*/
+
+console.table([personNumber, personName,personAddress,personId]);
