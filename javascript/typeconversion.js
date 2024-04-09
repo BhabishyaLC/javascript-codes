f7c3c7e94dc10e767ee512eddbc9514c021aa9ac
BhabishyaLC <163528869+BhabishyaLC@users.noreply.github.com>
2024-04-09 13:08:27 +0000

    typeconversions

---
+++ javascript/typeconversion.js
@@ -0,0 +1,8 @@
+let num1 = "12";
+let num2 ="13"
+let num3= "14";
+console.log(Number(num1) + Number(num2))//we have 
+//"String", "Boolean" just like "Number"
+console.log(Number(num1) + Number(num2) +num3 )
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
