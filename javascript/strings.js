813b0f9cd47421f929f87f066bccf5e7e75c1199
BhabishyaLC <163528869+BhabishyaLC@users.noreply.github.com>
2024-04-12 13:23:20 +0000

    javascript-updated

---
+++ javascript/strings.js
@@ -0,0 +1,17 @@
+const name= "Bhabishya"
+console.log(`Hello my name is ${name}`)
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.charAt('4'))
+console.log(name.indexOf("i"))
+const StringName= name.substring(0,4)
+console.log(StringName)
+const StringName1=name.slice(2,6)
+console.log(StringName1)
+const name2= "   bhabishya   "
+console.log(name2)
+console.log(name2.trim())
+const name3="hello world"
+console.log(name3.replace("l", "a"))
+console.log(name3.includes('e'))
